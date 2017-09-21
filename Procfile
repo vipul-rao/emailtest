c1115c4f7de3fa647fcd1dfce99feb5601129755
Arvind Mahto <mahto.arvin@gmail.com>
2017-09-21 23:12:02 +0000

    initial test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app:app -w 4 -t 12000
--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return "HELLO WORLD!"
+	
+	
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080,debug=True)
 
