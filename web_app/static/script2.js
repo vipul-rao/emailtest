81fc5ecfa2de3cf278f4fc986a6e424cb236c8c3
Arvind Mahto <mahto.arvin@gmail.com>
2017-09-26 22:50:29 +0000

    background

--- web_app/static/script2.js
+++ web_app/static/script2.js
@@ -1,29 +1,6 @@
 
 $(document).ready(function(){
-  $(window).scroll(function(){
-  	var scroll = $(window).scrollTop();
-	  if (scroll > 30) {
-          $(".home-menu").css("background" , "##00d2ff");
-          $(".home-menu").css("background" , "-webkit-linear-gradient(to right, #928DAB, #00d2ff)");
-          $(".home-menu").css("background" , "linear-gradient(to right, #928DAB, #00d2ff)");
-    
-
-          $(".home-menu").css("color","#fff");
-          $(".home-menu a").css("color","#fff");
-         
-
-
-	    
-	  }
-
-	  else{
-		$(".home-menu").css("background" , "white");
-        $(".home-menu").css("color","#2d3e50");
-        $(".home-menu a").css("color","#2d3e50");
-        
-	  }
-  })
-
+  
     $("#nav").localScroll({duration:800});
     $("#search-btn").scrollTo("#login",800);
     
